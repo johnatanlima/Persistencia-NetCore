7f225d637bbc9ec9abc7f540b9d05d6dca960e18
noteDexter <joh23nathan@gmail.com>
2019-09-15 23:58:45 -0300

    Scaffold base dados ok

---
+++ Entities/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistencia_NetCore.Entities
+{
+    public partial class Actor
+    {
+        public Actor()
+        {
+            FilmActor = new HashSet<FilmActor>();
+        }
+
+        public int ActorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ICollection<FilmActor> FilmActor { get; set; }
+    }
+}
---
+++ Entities/moviesContext.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Persistencia_NetCore.Entities
+{
+    public partial class moviesContext : DbContext
+    {
+        public moviesContext()
+        {
+        }
+
+        public moviesContext(DbContextOptions<moviesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Actor> Actor { get; set; }
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Film> Film { get; set; }
+        public virtual DbSet<FilmActor> FilmActor { get; set; }
+        public virtual DbSet<FilmCategory> FilmCategory { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=localhost;userid=root;pwd=;port=3306;database=movies;sslmode=none;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor>(entity =>
+            {
+                entity.ToTable("actor");
+
+                entity.Property(e => e.ActorId).HasColumnType("int(11)");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnType("varchar(45)");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnType("varchar(45)");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("category");
+
+                entity.Property(e => e.CategoryId).HasColumnType("int(11)");
+
+                entity.Property(e => e.Name).HasColumnType("varchar(45)");
+            });
+
+            modelBuilder.Entity<Film>(entity =>
+            {
+                entity.ToTable("film");
+
+                entity.Property(e => e.FilmId).HasColumnType("int(11)");
+
+                entity.Property(e => e.Description).HasColumnType("text");
+
+                entity.Property(e => e.Rating).HasColumnType("varchar(45)");
+
+                entity.Property(e => e.ReleaseYear).HasColumnType("int(11)");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnType("varchar(255)");
+            });
+
+            modelBuilder.Entity<FilmActor>(entity =>
+            {
+                entity.HasKey(e => new { e.FilmId, e.ActorId })
+                    .HasName("PRIMARY");
+
+                entity.ToTable("film_actor");
+
+                entity.HasIndex(e => e.ActorId)
+                    .HasName("film_actor_actor_idx");
+
+                entity.Property(e => e.FilmId).HasColumnType("int(11)");
+
+                entity.Property(e => e.ActorId).HasColumnType("int(11)");
+
+                entity.HasOne(d => d.Actor)
+                    .WithMany(p => p.FilmActor)
+                    .HasForeignKey(d => d.ActorId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("film_actor_actor");
+
+                entity.HasOne(d => d.Film)
+                    .WithMany(p => p.FilmActor)
+                    .HasForeignKey(d => d.FilmId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("film_actor_film");
+            });
+
+            modelBuilder.Entity<FilmCategory>(entity =>
+            {
+                entity.HasKey(e => new { e.FilmId, e.CategoryId })
+                    .HasName("PRIMARY");
+
+                entity.ToTable("film_category");
+
+                entity.HasIndex(e => e.CategoryId)
+                    .HasName("film_category_category_fk_idx");
+
+                entity.Property(e => e.FilmId).HasColumnType("int(11)");
+
+                entity.Property(e => e.CategoryId).HasColumnType("int(11)");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.FilmCategory)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("film_category_category_fk");
+
+                entity.HasOne(d => d.Film)
+                    .WithMany(p => p.FilmCategory)
+                    .HasForeignKey(d => d.FilmId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("film_category_film_fk");
+            });
+        }
+    }
+}
